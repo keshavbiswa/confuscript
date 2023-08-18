9d9d1e1df1fd458fa64fe89b9c5e66297850e8fb
Keshav Biswa <keshavbiswa21@gmail.com>
2023-08-18 17:39:29 +0530

    initial

---
+++ test/test_confuscript.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestConfuscript < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Confuscript::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
