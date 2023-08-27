aa56016fc2eac3301eae125f0a13dc56b940d598
Keshav Biswa <keshavbiswa21@gmail.com>
2023-08-27 22:56:54 +0530

    fixed block nodes

--- lib/confuscript/nodes/block_node.rb
+++ lib/confuscript/nodes/block_node.rb
@@ -2,15 +2,19 @@ module Confuscript
   module Nodes
     class BlockNode < BaseNode
       def evaluate(context)
-        action_list.elements.each do |element|
+        action_lists.each do |action|
+          element = action.elements.first
           element.evaluate(context) if element.respond_to?(:evaluate)
         end
       end
 
       private
 
-      def action_list
-        elements[2]
+      # The block before and after the { and } respectively
+      # This keeps on changing everytime I change the grammar
+      # Will need to revisit this again
+      def action_lists
+        elements[2].elements
       end
     end
   end
