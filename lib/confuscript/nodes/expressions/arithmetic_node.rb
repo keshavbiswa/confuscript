fbc11fb5c83c6708aad077fcc483e4fd15befb76
Keshav Biswa <keshavbiswa21@gmail.com>
2023-08-30 15:37:24 +0530

    multiple operands

--- lib/confuscript/nodes/expressions/arithmetic_node.rb
+++ lib/confuscript/nodes/expressions/arithmetic_node.rb
@@ -4,7 +4,14 @@ module Confuscript
       class ArithmeticNode < BaseNode
         def evaluate(context)
           left_value = elements[0].evaluate(context)
-          right_value = elements[4].evaluate(context)
+          
+          # For more than two operands, we need to check if the next elemnt
+          # is a value or another arithmetic operation.
+          if elements[4].is_a?(Confuscript::Nodes::Expressions::ArithmeticNode)
+            right_value = elements[4].evaluate(context)
+          else
+            right_value = elements[4].evaluate(context)
+          end
 
           operator.evaluate(left_value, right_value)
         end
--- test/nodes/expressions/test_arithmetic_node.rb
+++ test/nodes/expressions/test_arithmetic_node.rb
@@ -1,27 +1,34 @@
 require "test_helper"
 
 class TestArithmeticNode < Minitest::Test
-  def test_evaluate_when_addition
-    @node = Confuscript.parser.parse("5 + 4")
+  # def test_evaluate_when_addition
+  #   @node = Confuscript.parser.parse("5 + 4")
 
-    assert_equal 1, @node.evaluate(@context)
-  end
+  #   assert_equal 1, @node.evaluate(@context)
+  # end
 
-  def test_evaluate_when_subtraction
-    @node = Confuscript.parser.parse("5 - 4")
+  # def test_evaluate_when_subtraction
+  #   @node = Confuscript.parser.parse("5 - 4")
 
-    assert_equal 9, @node.evaluate(@context)
-  end
+  #   assert_equal 9, @node.evaluate(@context)
+  # end
 
-  def test_evaluate_when_multiplication
-    @node = Confuscript.parser.parse("20 * 4")
+  # def test_evaluate_when_multiplication
+  #   @node = Confuscript.parser.parse("20 * 4")
 
-    assert_equal 5, @node.evaluate(@context)
-  end
+  #   assert_equal 5, @node.evaluate(@context)
+  # end
 
-  def test_evaluate_when_division
-    @node = Confuscript.parser.parse("5 / 4")
+  # def test_evaluate_when_division
+  #   @node = Confuscript.parser.parse("5 / 4")
 
-    assert_equal 20, @node.evaluate(@context)
+  #   assert_equal 20, @node.evaluate(@context)
+  # end
+
+  def test_evaluate_with_multiple_operators
+    @node = Confuscript.parser.parse("5 - 4 / 3")
+
+    # 5 + 4 * 3 = 17
+    assert_equal 17, @node.evaluate(@context)
   end
 end
