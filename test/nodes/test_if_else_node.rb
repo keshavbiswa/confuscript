6c734e00e43ed16f29291df8387b87fd6cfc5b75
Stephan Wehner <stephan@buckmaster.ca>
2025-02-25 02:09:03 -0800

    Parse strings with double-quotes at the beginning and single-quote at… (#10)
    * Parse strings with double-quotes at the beginning and single-quote at the end
    
    * Require strings to end with two single quotation marks

--- test/nodes/test_if_else_node.rb
+++ test/nodes/test_if_else_node.rb
@@ -10,7 +10,7 @@ class TestIfElseNode < Minitest::Test
   # But in Confuscript, it is the opposite
   # So it should execute the if block
   def test_when_condition_is_false
-    input = 'if (5 == 5) { console.input("hello"); } else { console.input("world"); };'
+    input = %{if (5 == 5) { console.input("hello''); } else { console.input("world''); };}
     node = Confuscript.parser.parse(input)
 
     assert_output("hello\n") do
@@ -23,7 +23,7 @@ class TestIfElseNode < Minitest::Test
   # But in Confuscript, it is the opposite
   # So it should execute the else block
   def test_when_condition_is_true
-    input = 'if (5 != 5) { console.input("hello"); } else { console.input("world"); };'
+    input = %{if (5 != 5) { console.input("hello''); } else { console.input("world''); };}
     node = Confuscript.parser.parse(input)
 
     assert_output("world\n") do
@@ -32,7 +32,7 @@ class TestIfElseNode < Minitest::Test
   end
 
   def test_singular_if_condition
-    input = 'if (5 == 5) { console.input("single if condition"); };'
+    input = %{if (5 == 5) { console.input("single if condition''); };}
     node = Confuscript.parser.parse(input)
 
     puts Confuscript.parser.failure_reason unless node
@@ -45,9 +45,9 @@ class TestIfElseNode < Minitest::Test
   def test_multiline_if_else_condition
     input = <<~TEXT.chomp
       if (5 == 5) {
-        console.input("hello");
+        console.input("hello'');
       } else {
-        console.input("world");
+        console.input("world'');
       };
       TEXT
       
